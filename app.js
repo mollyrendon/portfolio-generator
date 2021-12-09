37654d9b9a7cffb1dbf8260d00d8464307b7cc19
Molly Wobbles <molly.rendon@outlook.com>
2021-12-09 13:21:50 -0500

    Updated template

--- app.js
+++ app.js
@@ -132,7 +132,13 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+      });
   });
 
 
